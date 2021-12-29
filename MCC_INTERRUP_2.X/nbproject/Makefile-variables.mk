3a5a33e2355cfccbe02e9a27152505d197283ca9
Francisco Motta <jmottaclaudio@gmail.com>
2021-12-29 14:43:53 -0500

    ints 3

---
+++ MCC_INTERRUP_2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MCC_INTERRUP_2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MCC_INTERRUP_2.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mccinterrup2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mccinterrup2.x.tar
