84e1f3c86c01ba24964375dfb121ff05ff86f4fb
Francisco Motta <jmottaclaudio@gmail.com>
2021-12-29 15:05:38 -0500

    PARO INT MCC

---
+++ MCC_INTERRUPCIONES_3_PARO.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MCC_INTERRUPCIONES_3_PARO.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MCC_INTERRUPCIONES_3_PARO.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mccinterrupciones3paro.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mccinterrupciones3paro.x.tar
